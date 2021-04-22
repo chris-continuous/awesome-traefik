2bb26dc432a4cad2285c5a078f20a7f167ba6d26
Your Name <you@example.com>
2021-04-22 08:54:45 +0000

    image version parameter

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,9 @@ export NEXTCLOUD_ADMIN_USER=admin
 export NEXTCLOUD_ADMIN_PASSWORD=mypassword 
 export STACK_NAME=nextcloud-$STACK
 export NEXTCLOUD_INSTANCE_PATH=nextcloud
+export IMAGE_VERSION=latest
 
-docker build -t elisabeth-nextcloud:$STACK ./nextcloud/custom-image
+docker build -t elisabeth-nextcloud:$IMAGE_VERSION ./nextcloud/custom-image
 docker stack deploy -c ./nextcloud/docker-compose.yaml $STACK_NAME
 
 export DB_HOST=10.10.1.28
@@ -24,6 +25,7 @@ export NEXTCLOUD_ADMIN_USER=admin
 export NEXTCLOUD_ADMIN_PASSWORD=mypassword 
 export STACK_NAME=nextcloud-$STACK
 export NEXTCLOUD_INSTANCE_PATH=nextcloud
+export IMAGE_VERSION=latest
 
-docker build -t elisabeth-nextcloud:$STACK ./nextcloud/custom-image
+docker build -t elisabeth-nextcloud:$IMAGE_VERSION ./nextcloud/custom-image
 docker stack deploy -c ./nextcloud/docker-compose.yaml $STACK_NAME
